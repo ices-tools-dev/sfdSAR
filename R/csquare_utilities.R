4a6f005deb966e9cc4c25c688a08d5b92e5293b4
Colin Millar <colinpmillar@gmail.com>
2018-11-14 14:19:24 +0100

    initial commit of package

---
+++ R/csquare_utilities.R
@@ -0,0 +1,56 @@
+#' Get information related to a C-Square
+#'
+#' Extract the surface area, latitude or longitude of a 0.05 resolution
+#' C-Square.
+#'
+#' @param csquare the name of a 0.05 resolution C-Square.
+#'
+#' @return A vector of numeric values: latitudes, longitudes or areas.
+#'
+#'
+#' @examples
+#' csquare_area("1501:370:370:1")
+#' csquare_lat("1501:370:370:1")
+#' csquare_lon("1501:370:370:1")
+#'
+#' @rdname csquare-functions
+#' @name csquare-functions
+NULL
+
+#' @rdname csquare-functions
+#' @export
+csquare_area <- function(csquare) {
+  lat <- csquare_lat(csquare)
+  cos(lat * pi / 180) * 111.1942^2 / 400
+}
+
+#' @rdname csquare-functions
+#' @export
+csquare_lat <- function(csquare) {
+  C41 <- as.numeric(substring(csquare, 14, 14))
+	C38 <- as.numeric(substring(csquare, 1, 1))
+	D38 <- as.numeric(substring(csquare, 2, 2))
+	D39 <- as.numeric(substring(csquare, 7, 7))
+	D40 <- as.numeric(substring(csquare, 11, 11))
+
+	G41 <- round(C41 * 2, -1) / 10
+	B40 <- round(abs(C38 - 4) * 2, -1) / 5 - 1
+
+	(D38 * 10 + D39 + D40 * 0.1 + G41 * 0.05 + 0.025)*B40
+}
+
+#' @rdname csquare-functions
+#' @export
+csquare_lon <- function(csquare) {
+  C41 <- as.numeric(substring(csquare, 14, 14))
+	C38 <- as.numeric(substring(csquare, 1, 1))
+	E38 <- as.numeric(substring(csquare,3,4))
+	E39 <- as.numeric(substring(csquare,8,8))
+	E40 <- as.numeric(substring(csquare,12,12))
+
+	H41 <- (round((C41 - 1) / 2, 1) - floor((C41 - 1) / 2)) * 2
+
+	B41 <- 1 - 2 * round(C38, -1) / 10
+
+	(E38 * 10 + E39 + 40 * 0.1 + H41 * 0.05 + 0.025) * B41
+}
---
+++ data-raw/gear_widths.R
@@ -0,0 +1,42 @@
+
+library(RODBC)
+library(dplyr)
+
+# get benthis auxiliary info
+dbConnection <- 'Driver={SQL Server};Server=SQL06;Database=VMS;Trusted_Connection=yes'
+conn <- odbcDriverConnect(connection = dbConnection)
+gear_widths <- sqlFetch(conn, "tblAux_benthisGearWidthsForWGSFD17")
+odbcClose(conn)
+
+# reference for model and params:
+#
+# Eigaard,O.R.,Bastardie,F.,Breen,M.,Dinesen, G.E.,Hintzen,N.T.,Laffargue,P.,Mortensen,L.O.,Nielsen,J.R.,Nilsson, Hans C.,
+# O’Neill, F. G., Polet, H., Reid, David G., Sala, A., Sko ¨ld, M., Smith, C., Sørensen, T. K., Tully, O., Zengin, M., and Rijnsdorp, A. D.
+# Estimating seabed pressure from demersal trawls, seines, and dredges based on gear design and dimensions. – ICES Journal
+# of Marine Science, 73: i27–i43.
+
+# reference for subsurface proportion and gear width:?
+#
+
+# gear width fill in type
+gearFillin <-
+  rbind(
+    data.frame(model = "a * avg_oal + b",
+               benthis_met = c('OT_MIX_DMF_BEN', 'OT_MIX_CRU_DMF', 'OT_SPF')),
+    data.frame(model = "a * b ^ avg_kw",
+               benthis_met = c('OT_CRU', 'OT_DMF', 'OT_MIX', 'OT_MIX_CRU',
+                               'TBB_CRU', 'TBB_DMF', 'SDN_DMF')),
+    data.frame(model = "a * b ^ avg_oal",
+               benthis_met = c('OT_MIX_DMF_PEL', 'TBB_MOL', 'DRB_MOL','SSC_DMF'))
+  )
+
+# join widths and lookup
+gear_widths <-
+  gear_widths %>%
+  filter(!is.na(FirstFactor)) %>%
+  rename(a = FirstFactor, b = SecondFactor) %>%
+  select(benthis_met, subsurface_prop, gearWidth, a, b) %>%
+  right_join(gearFillin, by = c("benthis_met" = "benthis_met"))
+
+# save data for use in package
+usethis::use_data(gear_widths, overwrite = TRUE)
